f2ad3aa39c3627d3d57abb65be0245be6c193775
kazadi <kazadi@lionhou.se>
2015-11-25 08:52:00 -0600

    Added missing excercises from PC

---
+++ ex11.py
@@ -0,0 +1,14 @@
+# print "How old are you?",
+# age = raw_input()
+# print "How tall are you?",
+# height = raw_input()
+# print "How much do you weight?"
+# weight = raw_input()
+
+age = input("How old are you?")
+height = input("How tall are you?")
+weight = input("How much do you weight?")
+
+
+print "So, your %r old, %r tall and %r heavy." % (
+	age, height, weight)
---
+++ ex13.py
@@ -0,0 +1,10 @@
+# Paremeters, Unpacking, Variables
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "This script is called: ", script
+print "Your first variable is: ", first
+print "Your second variable is: ", second
+print "Your third variable is: ", third
