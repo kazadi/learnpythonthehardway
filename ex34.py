be25ee9716cfdf3271a7b153a92879701b292134
kazadi <kazadi@lionhou.se>
2015-12-17 21:05:21 -0600

    Complete ex33-35. Created ex38

---
+++ ex34.py
@@ -0,0 +1,17 @@
+# Ex34: Accessing Elements of Lists
+
+animals = ['bear','python','peacock','kangaroo','whale','platypus']
+
+
+"""
+
+The animal at 1 is python
+The 3rd animal is peacock
+The first animal is bear
+The animal at 3 is kangaroo
+The fifth animal is whale
+The animal at 2 is peacock
+The sixth animal is platypus
+The animal at 4 whale
+
+"""
