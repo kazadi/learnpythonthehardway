39e87f1f28a5012ae7e65e4977f49812c5576438
kazadi <kazadix2@gmail.com>
2015-12-20 21:22:54 -0600

    Completed up to ex39. Still have to finish other half.

---
+++ ex39_2.py
@@ -0,0 +1,31 @@
+import hashmap
+
+#create a mapping of state to abbreviation
+states = hashmap.new()
+hashmap.set(states, 'Oregon', 'OR')
+hashmap.set(states, 'Florida', 'FL')
+hashmap.set(states, 'California', 'CA')
+hashmap.set(states, 'New York', 'NY')
+hashmap.set(states, 'Michigan', 'MI')
+
+# create a basic set of states some some cities in them
+cities = hashmap.new()
+hashmap.set(cities, 'CA', 'San Francisco')
+hashmap.set(cities, 'MI', 'Detroit')
+hashmap.set(cities, 'FL', 'Jacksonville')
+
+# add some more cities
+hashmap.set(cities, 'NY', 'New York')
+hashmap.set(cities, 'OR', 'Portland')
+
+# print out some cities
+print '-' * 10
+print "NY State has: %s" % hashmap.get(cities, 'NY')
+print "OR State has: %s" % hashmap.get(cities, 'OR')
+
+# print some states
+print '-' * 10
+print "Michigan's abbreviation is: %s" % hashmap.get(states, 'Michigan')
+print "Florida's abbreviation is: %s" % hashmap.get(states, Florida)
+
+# START BACK ON LINE 32
